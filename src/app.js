1bb86375f6c92c66ed98ee109dfbcc62480b5ef7
Yagnesh Modh <yagnesh.ash.modh@gmail.com>
2019-11-06 17:46:58 +0530

    commit code

--- src/app.js
+++ src/app.js
@@ -1,86 +1,131 @@
-import React, { PureComponent } from "react";
-import ErrorBoundary from "./components/ErrorBoundary";
+import React, { useState, memo } from "react";
 import styles from "./styles";
 
-export default class app extends PureComponent {
-  state = {
-    username: "",
-    password: ""
+const App = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  // const [user, setUser] = useState({
+  //   username: '',
+  //   password: ''
+  // })
+
+  const submit = e => {
+    e.preventDefault();
+    console.log(username, password);
   };
 
-  constructor(props) {
-    super(props);
-    console.log("constructor");
-  }
+  return (
+    <div>
+      <h1 style={styles.h1}>Login</h1>
+      {/* <h3 id="h3">{this.state.greet}</h3> */}
+      <form onSubmit={submit}>
+        <input
+          type="text"
+          name="username"
+          placeholder="Username"
+          value={username}
+          onChange={e => setUsername(e.target.value)}
+        />
+        <input
+          type="password"
+          name="password"
+          placeholder="Password"
+          value={password}
+          onChange={e => setPassword(e.target.value)}
+        />
+        <h1>{`Username: ${username}`}</h1>
+        <h1>{`Password: ${password}`}</h1>
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
+};
 
-  static getDerivedStateFromProps(nextProps, prevState) {
-    console.log("getDerivedStateFromProps");
-    return {
-      greet: `Hello ${nextProps.name}`
-    };
-  }
+export default memo(App);
 
-  componentDidMount() {
-    document.getElementById("h3").style.color = "green";
-    document.addEventListener("copy", this.copyElement);
-  }
+// import React, { PureComponent } from "react";
+// import ErrorBoundary from "./components/ErrorBoundary";
+// import styles from "./styles";
 
-  shouldComponentUpdate(nextProps, nextState) {
-    throw new Error("Booom....");
-    // if(this.props.name !== nextProps.name) {
-    //   return true
-    // }
-    // return false
-    return true;
-  }
+// export default class app extends PureComponent {
+//   state = {
+//     username: "",
+//     password: ""
+//   };
 
-  getSnapshotBeforeUpdate(prevProps, prevState) {
-    return true;
-  }
+//   constructor(props) {
+//     super(props);
+//     console.log("constructor");
+//   }
 
-  componentWillUnmount() {
-    document.removeEventListener("copy", this.copyElement);
-  }
+//   static getDerivedStateFromProps(nextProps, prevState) {
+//     console.log("getDerivedStateFromProps");
+//     return {
+//       greet: `Hello ${nextProps.name}`
+//     };
+//   }
 
-  componentDidUpdate(prevProps, prevState, snapshot) {}
+//   componentDidMount() {
+//     document.getElementById("h3").style.color = "green";
+//     document.addEventListener("copy", this.copyElement);
+//   }
 
-  copyElement = () => {
-    console.log("Copied...");
-  };
+//   shouldComponentUpdate(nextProps, nextState) {
+//     throw new Error("Booom....");
+//     // if(this.props.name !== nextProps.name) {
+//     //   return true
+//     // }
+//     // return false
+//     return true;
+//   }
 
-  onChangeText = event => {
-    this.setState({ [event.target.name]: event.target.value });
-  };
+//   getSnapshotBeforeUpdate(prevProps, prevState) {
+//     return true;
+//   }
 
-  submit = event => {
-    event.preventDefault();
-    console.log(this.username, this.password);
-  };
+//   componentWillUnmount() {
+//     document.removeEventListener("copy", this.copyElement);
+//   }
 
-  render() {
-    console.log("render");
-    return (
-      <div>
-        <h1 style={styles.h1}>Login</h1>
-        <h3 id="h3">{this.state.greet}</h3>
-        <form onSubmit={this.submit}>
-          <input
-            type="text"
-            name="username"
-            placeholder="Username"
-            onChange={this.onChangeText}
-          />
-          <input
-            type="password"
-            name="password"
-            placeholder="Password"
-            onChange={this.onChangeText}
-          />
-          <h1>{`Username: ${this.state.username}`}</h1>
-          <h1>{`Password: ${this.state.password}`}</h1>
-          <button type="submit">Login</button>
-        </form>
-      </div>
-    );
-  }
-}
+//   componentDidUpdate(prevProps, prevState, snapshot) {}
+
+//   copyElement = () => {
+//     console.log("Copied...");
+//   };
+
+//   onChangeText = event => {
+//     this.setState({ [event.target.name]: event.target.value });
+//   };
+
+//   submit = event => {
+//     event.preventDefault();
+//     console.log(this.username, this.password);
+//   };
+
+//   render() {
+//     console.log("render");
+//     return (
+//       <div>
+//         <h1 style={styles.h1}>Login</h1>
+//         <h3 id="h3">{this.state.greet}</h3>
+//         <form onSubmit={this.submit}>
+//           <input
+//             type="text"
+//             name="username"
+//             placeholder="Username"
+//             onChange={this.onChangeText}
+//           />
+//           <input
+//             type="password"
+//             name="password"
+//             placeholder="Password"
+//             onChange={this.onChangeText}
+//           />
+//           <h1>{`Username: ${this.state.username}`}</h1>
+//           <h1>{`Password: ${this.state.password}`}</h1>
+//           <button type="submit">Login</button>
+//         </form>
+//       </div>
+//     );
+//   }
+// }
